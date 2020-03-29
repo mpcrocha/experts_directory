7b86d0aa9dfee8afbc8cec3dfc230c6c9bb58a46
marcos_rocha <marcos.rocha@m4u.com.br>
2020-03-29 17:41:54 -0400

    The interface should list all members with their name, short url and the number of friends.

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,5 +2,10 @@
 
 class Member < ApplicationRecord
   has_many :headings
+  has_many :friendships
   has_many :members, through: :friendships
+
+  def friends_lists
+    Friendship.where(friend: self).or(Friendship.where(member: self))
+  end
 end
